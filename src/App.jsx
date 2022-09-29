62770025ee3a3fe6996e41d576e31f51d2ff3f75
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2022-09-28 21:11:22 -0300

    utilizando CHAKRA

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Hello from '../src/components/Hello'
 import './App.css'
-import pizza from '../src/img/pizza-steve.png';
+import Index from './components/Index'
+import { ChakraProvider } from '@chakra-ui/react'
+import Hello from './components/Hello'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://static.wikia.nocookie.net/universosteven/images/1/1f/Pizza_Steve.png/revision/latest?cb=20150410231308&path-prefix=es" target="_blank">
-          <img src={pizza} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <ChakraProvider>
+
+
+      <Index/>
       <Hello/>
-    </div>
+
+      
+  
+    </ChakraProvider>
   )
 }
 
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,11 +1,92 @@
 import React from 'react'
+import { Center, Square, Circle,Heading } from '@chakra-ui/react'
+import { Button, ButtonGroup} from '@chakra-ui/react'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { IconButton } from '@chakra-ui/react'
+import { PhoneIcon, AddIcon, WarningIcon,SearchIcon } from '@chakra-ui/icons'
+import { Avatar, AvatarBadge, AvatarGroup } from '@chakra-ui/react'
+import { Wrap, WrapItem } from '@chakra-ui/react'
+import { Flex, Spacer } from '@chakra-ui/react'
+import { Switch } from '@chakra-ui/react'
+import { Code } from '@chakra-ui/react'
+
+import {
+Drawer,
+DrawerBody,
+DrawerFooter,
+DrawerHeader,
+DrawerOverlay,
+DrawerContent,
+DrawerCloseButton,
+} from '@chakra-ui/react'
+
+
 
 const Hello = () => {
-  return (
-    <div>
-        <h1>Hola me llamo Exequiel Portero</h1>
-    </div>
-  )
+return (
+<div>
+  <Center bg='tomato' w='50rem' h='200px' color='white'>
+    <h1>Exequiel</h1>
+    <Button colorScheme='green' size="lg">Button</Button>
+  </Center>
+
+  <Stack direction='row' spacing={4}>
+    <Button isLoading colorScheme='teal' variant='solid'>
+      Email
+    </Button>
+    <Button isLoading loadingText='Submitting' colorScheme='teal' variant='outline'>
+      Submit
+    </Button>
+  </Stack>
+
+
+  <IconButton aria-label='Search database' colorScheme='blue' icon={<SearchIcon />} />
+  <Heading noOflines={1}>Hola me llamo portero daniel exequiel</Heading>
+
+
+  <Flex direction="row" justify="center" align="center">
+
+    <Wrap>
+      <WrapItem>
+        <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' />
+      </WrapItem>
+      <WrapItem>
+        <Avatar name='Kola Tioluwani' src='https://bit.ly/tioluwani-kolawole' />
+      </WrapItem>
+      <WrapItem>
+        <Avatar name='Kent Dodds' src='https://bit.ly/kent-c-dodds' />
+      </WrapItem>
+      <WrapItem>
+        <Avatar name='Ryan Florence' src='https://bit.ly/ryan-florence' />
+      </WrapItem>
+      <WrapItem>
+        <Avatar name='Prosper Otemuyiwa' src='https://bit.ly/prosper-baba' />
+      </WrapItem>
+      <WrapItem>
+        <Avatar name='Christian Nwamba' src='https://bit.ly/code-beast' />
+      </WrapItem>
+      <WrapItem>
+        <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
+      </WrapItem>
+    </Wrap>
+
+  </Flex>
+
+  <Stack align='center' direction='row'>
+  <Switch size='sm' />
+  <Switch size='md' />
+  <Switch size='lg' />
+</Stack>
+
+<Code children="console.log(welcome)" colorScheme="red"/>
+
+
+
+</div>
+
+
+
+)
 }
 
 export default Hello
